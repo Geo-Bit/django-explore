df746cd2a35c577556d235a17e939db0c6460526
George Tipton <gtip22@gmail.com>
2023-01-12 10:01:11 -0700

    Added test cases for question dates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.template import loader
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -57,8 +58,9 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        # Return the last five published questions.
-        return Question.objects.order_by('-pub_date')[:5]
+        # Return the last five published questions (not including those set to be published in the future).
+        #return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5] # Fixed return to exclude future questions
 
 class DetailView(generic.DetailView):
     model = Question
