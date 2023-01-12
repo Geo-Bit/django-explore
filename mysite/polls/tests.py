3f23f8143e4c553397f195cfb059ab33a7cf9b84
George Tipton <gtip22@gmail.com>
2023-01-12 10:11:48 -0700

    Added question tests for future questions

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -11,7 +11,26 @@ def create_question(question_text,days):
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text,pub_date=time)
 
-class QuestionINdexViewTests(TestCase):
+class QuestionDetailViewTests(TestCase):
+    def test_future_questions(self):
+        """
+        The detail view of a question with a pub_date in the future returns a 404 not found.
+        """
+        future_question = create_question(question_text="Future question.",days=5)
+        url = reverse('polls:detail',args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code,404)
+
+    def test_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past displays the question's text.
+        """
+        past_question = create_question(question_text="Past Question",days=-5)
+        url = reverse('polls:detail',args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
+class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
         """
         If no questions exist, an appropriate message is displayed.
@@ -66,7 +85,7 @@ class QuestionINdexViewTests(TestCase):
             [question2,question1],
         )
 
-class QuestionMOdelTests(TestCase):
+class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         """
         was_published_recently() returns False for questions whose pub_date is in the future.
