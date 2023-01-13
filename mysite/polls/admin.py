b2436c427ba7f100ab6f83c86108d5f909eeac52
George Tipton <gtip22@gmail.com>
2023-01-13 10:01:04 -0700

    Added Question Admin page functionality

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 from .models import Question, Choice
 
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline): # represent the choices in the question creation page.
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    #fields = ['pub_date','question_text'] # controls the order of the fields on the Question admin page.
+    fieldsets = [ # Use field sections
+        (None,               {'fields':['question_text']}),
+        ('Date Information', {'fields':['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text','pub_date','was_published_recently')
+    list_filter = ['pub_date'] # adds a filter sidebar to the Question Admin page for pub_date
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.utils import timezone
 from django.db import models
+from django.contrib import admin
 
 
 # Create your models here.
@@ -13,7 +14,16 @@ class Question(models.Model):
         now = timezone.now()
         #return self.pub_date >= timezone.now() - datetime.timedelta(days=1) <-- buggy code
         return now - datetime.timedelta(days=1) <= self.pub_date <= now # Fixed code
-        
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 class Choice(models.Model):
     question = models.ForeignKey(Question,on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
